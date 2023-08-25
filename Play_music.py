355adbba57f5d696ea21b1c3c5768c167d791f68
Pamingrid <pamingrid26@gmail.com>
2023-08-25 20:10:46 +0100

    play_music

---
+++ Play_music.py
@@ -0,0 +1,39 @@
+from replit import audio
+import os, time
+
+def play():
+  source = audio.play_file('audio.wav')
+  source.paused = False # unpause the playback
+  while True:
+    stop_music=int(input("Press 2 to exit: "))
+    if play == 2:
+      source.paused=True
+      return 
+    else:
+      continue
+# Start taking user input and doing something with itinput(" Do you want to play music")
+
+while True:
+  os.system("clear")# clear the screen
+  print("MyPOD Music Player")
+  time.sleep(5)
+  print("Press 1 to play")
+  time.sleep(5)
+  print("Press 2 to Exit ")
+  time.sleep(5)
+  print("Press anything else to see the menu again")
+  userInput = int(input())
+  if userInput == 1:
+    print("Playing music!")
+    play()
+  elif userInput == 2:
+    exit()
+  else :
+    continue
+
+
+  # Show the menu
+
+  # take user's input
+
+  # check whether you should call the play() subroutine depending on user's input
