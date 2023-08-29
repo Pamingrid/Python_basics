90a5bace12c0890da465bcbb12ee41409c017d54
Pamingrid <pamingrid26@gmail.com>
2023-08-29 20:50:26 +0100

    Learn how to use the print method

---
+++ Use_Print.py
@@ -0,0 +1,7 @@
+
+print("30 Days Down")
+count = 0
+for i in range (1, 31):
+    response = input("What have you done today?: ")
+    count+=1
+    print(f" You thought Day {i} activities was {response}")
