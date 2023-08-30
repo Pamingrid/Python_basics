93038e9c8a6a8e487e2e55ce203625026c2c3333
Pamingrid <pamingrid26@gmail.com>
2023-08-30 15:21:21 +0100

    Learn how to create list

---
+++ first_list.py
@@ -0,0 +1,4 @@
+import random
+greet = ["Bonjour", "Good morning", "Buenos días", "guten morgen", "saluté", "Mbembeu kiri", "Me tchasseù"]
+number= random.randint(0, len(greet))
+print(f"{greet[number]}")
