9a3988f6bd818f096c362ad697b0f4293e6dd67d
Pamingrid <pamingrid26@gmail.com>
2023-08-30 15:22:00 +0100

    learn how to create dynamic list

---
+++ Todolist.py
@@ -0,0 +1,24 @@
+import os, time
+to_do_list=[]
+
+def printList():
+   print()
+   for item in to_do_list:
+     print(item)
+     print()
+
+while True:
+  menu = input("Do you want to view, add, or edit your to do list ? : ")
+  if menu == "view":
+    printList()
+  elif menu == "add":
+    item = input("What should I add to the todo list? : ")
+    to_do_list.append(item)
+    print(f"Your actual todo list count :", {to_do_list})
+  elif menu == "edit":
+    item = input("What should I edit to the todo list? :")
+    if item in to_do_list:
+        to_do_list.remove(item)
+    print(f"The remaining activities are: {printList()}")
+    os.system("clear")
+    time.sleep(3)
