e68e23adadc390f70405ae48439d2eae187afef5
Pamingrid <pamingrid26@gmail.com>
2023-10-18 11:08:42 +0100

    challenge 100 code Day 48 and 49

---
+++ Day_49.py
@@ -0,0 +1,18 @@
+import os, time
+
+
+f= open("high.score", "r")
+scores=f.read().split("\n")
+f.close()
+highscore = 0
+name = None
+
+for rows in scores: 
+  data = rows.split()
+  if data != []:
+    if int(data[1])>highscore:
+      highscore= int(data[1])
+      name = data[0]
+  
+
+print(f"The winner is, {name}, with, {highscore}")
